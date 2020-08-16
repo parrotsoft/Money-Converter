c15a4512fda7da28779a31de6ce117e159c6194b
Miguel Lopez Ariza <mlopez@transportationamerica.com>
2020-08-15 19:11:22 -0500

    feat: primer commit

---
+++ popup.js
@@ -0,0 +1,38 @@
+$(function() {
+    const typeM = document.querySelector('#typeM');
+    const mount = document.querySelector('#value');
+    const result = document.querySelector('#result');
+    document.getElementById("btnConvertir").onclick = (e) => {
+        e.preventDefault();
+        // alert($('#value').cleanVal().val());
+        const type = validateType(typeM.value);
+        getData(type, mount.value);
+    }
+
+    function validateType(tp) {
+        if (tp == 1) {
+            // $('#value').mask("#.##0,00", {reverse: true, watchDataMask: true});
+            // $('#result').mask("#.##0,00", {reverse: true, watchDataMask: true});
+            return {
+                prop1: 'USD',
+                prop2: 'COP'
+            }
+        } else {
+            return {
+                prop1: 'COP',
+                prop2: 'USD'
+            }
+        }
+    }
+
+    function getData(type, mount) {
+        fetch(`http://api.wahrungsrechner.org/v1/quotes/${type.prop1}/${type.prop2}/json?qty=${mount}&key=4806|RB*~riho7yu*tRa0nvDpPvYKFu*cj2QB`).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            this.result.value = (mount*data.result.amount);
+        }).catch(function(error) {
+            alert('Error ', JSON.stringify(error));
+        })
+    }
+
+});
